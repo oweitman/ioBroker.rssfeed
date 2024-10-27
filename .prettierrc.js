55e4b1727d975fe905a211671802e710fd3bcd7c
oweitman <oweitman@gmx.de>
2024-10-27 18:41:42 +0100

    update jsonconfig responsive

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,7 @@ module.exports = {
     semi: true,
     trailingComma: "all",
     singleQuote: false,
-    printWidth: 120,
+    printWidth: 80,
     useTabs: false,
     tabWidth: 4,
     endOfLine: "lf",
